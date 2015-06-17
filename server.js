71ad138087cfcc568c5ac7b2200889e078f1b20a
Johan Lim <jjohanlim@gmail.com>
2015-06-17 11:43:11 +0200

    moved some variables

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
+require('dotenv').load();
 var app = require('express')();
 var passport = require('passport');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs'); 
+var url = require('url');
+var multer = require('multer');
 var cloudinary = require('cloudinary');
 var users = ['sarina', 'johan', 'anders', 'jc', 'alex'];
+var done = false;
 
+var cloudinary_vars = url.parse(process.env.CLOUDINARY_URL);
 
-//probably move to env later
 cloudinary.config({ 
-	cloud_name: 'fab', 
-	api_key: '685122177578734', 
-	api_secret: 'ajmGJfwrimKrdiZeTuF29V8QO34' 
+	cloud_name: cloudinary_vars.hostname, 
+	api_key: cloudinary_vars.auth.split(':')[0], 
+	api_secret: cloudinary_vars.auth.split(':')[1]
 });
 
 
@@ -22,8 +26,30 @@ app.get('/myimage', function(req, res){
 	res.sendFile(__dirname + '/my_image.html');
 });
 
+app.use(multer({ dest: './uploads/',
+	rename: function (fieldname, filename) {
+		return filename+Date.now();
+	},
+	onFileUploadStart: function (file) {
+		console.log(file.originalname + ' is starting ...')
+	},
+	onFileUploadComplete: function (file) {
+		sendToCloudinary(file);
+		done=true;
+	}
+}));
 
+app.post('/api/photo', function(req,res){
+	if(done==true){
+    	res.end("File uploaded.");
+  	}
+});
 
+function sendToCloudinary(file) {
+	cloudinary.uploader.upload('./uploads/'+file.name, function(result) { 
+		console.log(result);
+	});
+}
 // Stub for user authentication --> needs more thought
 app.get('/api/users/me', function(req, res) {
 	var user = users[Math.floor(Math.random()*users.length)];
