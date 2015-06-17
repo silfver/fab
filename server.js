4a783f6cc27c08a0e07524be33805c4dab15587f
Johan Lim <jjohanlim@gmail.com>
2015-06-17 14:16:14 +0200

    fixed namespace error

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ cloudinary.config({
 	api_secret: cloudinary_vars.auth.split(':')[1]
 });
 
-var nsp = false;
 var user = false;
+var nsp = io.of('/fab');
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/someone_elses_image.html');
@@ -58,20 +58,21 @@ app.get('/api/users/me', function(req, res) {
 	var user = createUser();
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({ username: user}));
-	nsp.on('connection', function(socket){
+	
+});
+
+nsp.on('connection', function(socket){
 		socket.on('image reaction', function(msg){
 			nsp.emit('image reaction', msg.user + " sa "+msg.reaction);
 		});
-	});	
-});
+});	
 function createUser() {
 	user = users[Math.floor(Math.random()*users.length)];
-	nsp = io.of('/'+user);
 	return user;
 }
 
 function sendImageToMyFriends(image) {
-	nsp.emit('new image', {image: image.secure_url});
+	nsp.emit('new image', {image: image.secure_url, user: user});
 }
 
 http.listen(process.env.PORT || 3000, function(){
