c9dae20ecb44a4eff243126f57487fca02da4f89
Johan Lim <jjohanlim@gmail.com>
2015-06-17 13:55:41 +0200

    ability to send images

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ cloudinary.config({
 	api_secret: cloudinary_vars.auth.split(':')[1]
 });
 
+var nsp = false;
+var user = false;
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/someone_elses_image.html');
@@ -48,24 +50,30 @@ app.post('/api/photo', function(req,res){
 function sendToCloudinary(file) {
 	cloudinary.uploader.upload('./uploads/'+file.name, function(result) { 
 		console.log(result);
+		sendImageToMyFriends(result);
 	});
 }
 // Stub for user authentication --> needs more thought
 app.get('/api/users/me', function(req, res) {
-	var user = users[Math.floor(Math.random()*users.length)];
+	var user = createUser();
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({ username: user}));
+	nsp.on('connection', function(socket){
+		socket.on('image reaction', function(msg){
+			nsp.emit('image reaction', msg.user + " sa "+msg.reaction);
+		});
+	});	
 });
+function createUser() {
+	user = users[Math.floor(Math.random()*users.length)];
+	nsp = io.of('/'+user);
+	return user;
+}
 
-var nsp = io.of('/sarina');
-nsp.on('connection', function(socket){
-	socket.on('image reaction', function(msg){
-	nsp.emit('image reaction', msg.user + " sa "+msg.reaction);
-	});
-	socket.on('request image', function(msg) {
-	nsp.emit('new image', { image: "http://res.cloudinary.com/fab/image/upload/c_fill,h_0.55,w_1.0/v1434446554/internal_images/tumblr_nm4y21eqTj1u53c30o1_1280.jpg"});
-	});
-});
+function sendImageToMyFriends(image) {
+	nsp.emit('new image', {image: image.secure_url});
+}
+
 http.listen(process.env.PORT || 3000, function(){
 	console.log('listening on process port');
 });
