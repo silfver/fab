46f14d96667a37eb507b273194131840381a4fec
Johan Lim <jjohanlim@gmail.com>
2015-06-25 14:51:18 +0200

    another bug in user schema

--- models/user.js
+++ models/user.js
@@ -1,12 +1,11 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var User = new Schema({
+
+module.exports = mongoose.model('User', {
     username: String,
     password: String,
     firstName: String,
     lastName: String,
     email: String,
     friends: [{ type: Schema.Types.ObjectId, ref: "User" }]
-});
-
-module.exports = User;
+})
