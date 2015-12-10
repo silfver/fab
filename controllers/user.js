5c9f91b7d2c47c83182f672c275bfe06cc67a5e9
Johan Lim <jjohanlim@gmail.com>
2015-12-10 19:07:57 +0100

    send html emails

--- controllers/user.js
+++ controllers/user.js
@@ -204,7 +204,7 @@ exports.forgotPassword = function(req, res, next) {
           subject: 'Reset your Fab! password',
           text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
             'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-            'fab://reset/' + token + '\n\n' +
+            '<a href="fab://reset/' + token + '">fab://reset/' + token + '</a>\n\n' +
             'If you did not request this, please ignore this email and your password will remain unchanged.\n'
         },
         function(err, info) {
